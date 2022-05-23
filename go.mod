0101457052fc53d7b6c83ed1ecafff7dec9e6978
Sergey <tsvetsv8@gmail.com>
2022-05-23 08:57:11 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jsdeveloper88/modex
+
+go 1.16
---
+++ modex.go
@@ -0,0 +1,8 @@
+package modex
+
+import "fmt"
+
+// Hi returns a friendly greeting
+func Hi(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
